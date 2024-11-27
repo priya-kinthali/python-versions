798f75c5453dc9a2dab519498f4aebee3be55ad3
priya-kinthali <priya-kinthali@github.com>
2024-11-27 19:25:00 +0530

    with open ssl

--- installers/macos-pkg-setup-template.sh
+++ installers/macos-pkg-setup-template.sh
@@ -68,16 +68,6 @@ fi
 
 chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJOR_MINOR python
 
-# Ensure OpenSSL is linked
-brew link openssl@3 --force
-# Echo the OpenSSL version
-echo "OpenSSL version:"
-/opt/homebrew/opt/openssl@3/bin/openssl version
-
-# Set environment variables for OpenSSL
-export LDFLAGS="-L/opt/homebrew/opt/openssl@3/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
 echo "Upgrading pip..."
 export PIP_ROOT_USER_ACTION=ignore
 ./python -m ensurepip
